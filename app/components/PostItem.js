37d18c39cec55fc950dfee3ef1ce7550eb1cbab4
arnuphapt <arnuphap.t@kkumail.com>
2024-07-26 22:11:36 +0700

    กฟไก

--- app/components/PostItem.js
+++ app/components/PostItem.js
@@ -1,38 +1,54 @@
-import Image from 'next/image';
-import React from 'react';
-
-const PLACEHOLDER = './Images/placeholder.jpg';
-
-function PostItem({ post, modal = false }) {
+import React from 'react'
+import { HiOutlineLocationMarker,HiOutlineCalendar } from "react-icons/hi";
+import UserInfo from './UserInfo';
+const PLACEHOLDER='./Images/placeholder.jpg'
+function PostItem({post,modal=false}) {
   return (
-    <div className="cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out bg-sky-50 p-2 rounded-3xl hover:bg-sky-200">
-      <div className="relative w-full h-40 overflow-hidden rounded-3xl">
-        <Image
+   <>
+{post?
+  <div className="max-w-sm bg-white border
+ border-gray-200 rounded-lg shadow cursor-pointer
+  dark:bg-gray-800 dark:border-gray-700 ">
+    
+        <img className="rounded-t-lg 
+        w-full h-[180px] object-cover" 
         src={post.image?post.image:PLACEHOLDER}
-        alt={post.title || 'banner'}
-          className="rounded-3xl"
-          fill
-          sizes="100vw"
-          style={{ objectFit: 'cover' }}
-        />
+         alt="banner" />
+   
+    <div className="p-5">
+       
+    <h5 className="mb-2 text-xl 
+    font-bold tracking-tight 
+    text-gray-900 dark:text-white">
+      {post.title}
+      </h5>
+      <div className='flex items-center text-orange-500 gap-2 mb-2'>
+        <HiOutlineCalendar className='text-[20px]'/>
+        {post.date}
       </div>
-      {post.title && (
-        <h2 className="font-bold text-[18px] mb-1 mt-2 line-clamp-2">
-          {post.title}
-        </h2>
-      )}
-      {post.viewCount !== undefined && (
-        <div className="mt-2 text-sm text-gray-600">
-          <span>Views: {post.viewCount}</span>
+      <div className='flex items-center 
+      text-blue-500 gap-2 mb-2'>
+        <HiOutlineLocationMarker className='text-[20px]'/>
+        {post.location}
+      </div>
+        <p className="mb-3 font-normal
+         text-gray-700 
+         dark:text-gray-400">
+          {post.desc}</p>
+
+      {!modal? <UserInfo user={post} />:null}
+      {modal? 
+     <div>
+      <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+            <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" ></path></svg>
+      </a>
         </div>
-      )}
-      {post.likes !== undefined && (
-        <div className="mt-2 text-sm text-gray-600">
-          <span>Likes: {post.likes.length}</span>
-        </div>
-      )}
+        :null}
     </div>
-  );
+</div>:null}
+</> 
+  )
 }
 
-export default PostItem;
+export default PostItem
