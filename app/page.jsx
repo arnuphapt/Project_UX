fd3b54c339d8cd49125f7c8f0330d724b0e22ea5
arnuphapt <arnuphap.t@kkumail.com>
2024-07-26 21:21:57 +0700

    dawdwa

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,48 @@
 "use client"
 
-import { collection, getDocs, getFirestore, query } from 'firebase/firestore';
 import app from './Shared/firebaseConfig';
-import { useEffect, useState } from 'react';
 import PinList from './components/Pins/PinList';
+import Head from 'next/head'
+import Image from 'next/image'
+import Posts from './components/Posts'
+import { getFirestore, doc, setDoc, getDoc, 
+  collection, getDocs, query, where } from "firebase/firestore";
+import { useEffect, useState } from 'react'
 export default function Home() {
   const db = getFirestore(app);
   const [listOfPins, setListOfPins] = useState([]);
-
+  const [posts,setPosts]=useState([])
+  useEffect(()=>{
+    getPost();
+  },[])
   useEffect(() => {
     getAllPins();
   }, [])
+
+  const getPost=async()=>{
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    querySnapshot.forEach((doc) => {
+    
+   setPosts(posts=>[...posts,doc.data()]);
+});
+  }
+  const onGamePress=async(gameName)=>{
+    setPosts([]);
+    if(gameName=='Other Games')
+    {
+      getPost();
+      return ;
+    }
+    const q=query(collection(db,"posts"),
+    where("game","==",gameName));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      let data=doc.data();
+      data.id=doc.id
+      setPosts(posts=>[...posts,doc.data()]);
+   
+});
+  }
   const getAllPins = async () => {
     const q = query(collection(db, 'pinterest-post'));
     const querySnapshot = await getDocs(q);
@@ -30,7 +62,8 @@ export default function Home() {
     <>
       <div className="p-4">
       <PinList listOfPins={listOfPins}  />
-      
+      {posts? <Posts posts={posts}/>:null}
+
       </div>
     </>
   )
