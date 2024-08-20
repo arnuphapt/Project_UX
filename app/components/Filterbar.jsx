5310648082c77f034e49c42e4dba579e988cc5b3
arnuphapt <arnuphap.t@kkumail.com>
2024-08-21 00:52:44 +0700

    icon filter update

--- app/components/Filterbar.jsx
+++ app/components/Filterbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Button, ButtonGroup } from "@nextui-org/react";
+import { FaProjectDiagram, FaPencilRuler, FaPaintBrush } from 'react-icons/fa';
+import { MdViewQuilt } from 'react-icons/md';
+import { SiWix, SiFigma } from 'react-icons/si';
+import { AiOutlineAppstore } from 'react-icons/ai';
 
 const Technology = [
-    { id: 1, name: 'Project' },
-    { id: 2, name: 'Ux' },
-    { id: 3, name: 'Ui' },
-    { id: 4, name: 'Wireframe' },
-    { id: 5, name: 'Wix' },
-    { id: 6, name: 'Figma' },
-
+    { id: 1, name: 'Project', icon: <FaProjectDiagram /> },
+    { id: 2, name: 'Ux', icon: <FaPencilRuler /> },
+    { id: 3, name: 'Ui', icon: <FaPaintBrush /> },
+    { id: 4, name: 'Wireframe', icon: <MdViewQuilt /> },
+    { id: 5, name: 'Wix', icon: <SiWix /> },
+    { id: 6, name: 'Figma', icon: <SiFigma /> },
 ];
 
 function FilterBar({ selectedTech, setSelectedTech }) {
@@ -29,12 +32,14 @@ function FilterBar({ selectedTech, setSelectedTech }) {
     return (
         <div className="flex flex-wrap justify-center mb-7">
             {['All', ...Technology.map(tech => tech.name)].map((tech, index) => (
-                <ButtonGroup key={tech}> {/* Add a unique key here */}
-                    <Button variant='light'
+                <ButtonGroup key={tech}>
+                    <Button
+                        variant='light'
                         onClick={() => toggleTech(tech)}
                         className={`mr-4 mb-2 px-3 py-2 border-b-3 ${(tech === 'All' && selectedTech.length === 0) || selectedTech.map(t => t.toLowerCase()).includes(tech.toLowerCase()) ? 'text-black'
                                 : 'text-gray-500 border-transparent hover:border-gray-300'}`}>
-                        {tech}
+                        {tech === 'All' ? <AiOutlineAppstore /> : Technology.find(t => t.name === tech)?.icon} {/* Icon */}
+                        <span className="ml-2">{tech}</span>
                     </Button>
                 </ButtonGroup>
             ))}
