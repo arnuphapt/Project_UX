36ba9f7cc00a29381a56cf2e109f8c35867a0a5d
arnuphapt <arnuphap.t@kkumail.com>
2024-08-22 14:51:31 +0700

    อนุญาติทุกโดเมน

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,18 +8,6 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    callbacks: {
-        async signIn({ user, account, profile }) {
-            const emailDomain = user.email.split('@')[1];
-            const allowedDomain = 'kkumail.com'; // โดเมนที่อนุญาตให้ล็อกอิน
-
-            if (emailDomain === allowedDomain) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
 });
 
 export { handler as GET, handler as POST };
