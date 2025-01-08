d3b198227227f7d0161452a60e9ca2560c8257a6
arnuphapt <arnuphap.t@kkumail.com>
2025-01-09 00:48:23 +0700

    แก้ layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 import Provider from "./Provider";
 
 const inter = Inter({ subsets: ["latin"] });
