13ee9f524ecb743d0265b4de4dd0a3caaa81c373
arnuphapt <arnuphap.t@kkumail.com>
2025-01-09 00:35:30 +0700

    เอาโค้ดไม่จำเป็นออก

--- app/page.jsx
+++ app/page.jsx
@@ -3,35 +3,14 @@ import { app } from './Shared/firebaseConfig';
 import PinList from './components/Pins/PinList';
 import { getFirestore, collection, getDocs, query } from "firebase/firestore";
 import { useEffect, useState } from 'react';
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox } from "@nextui-org/react";
 
 export default function Home() {
     const db = getFirestore(app);
     const [listOfPins, setListOfPins] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [showModal, setShowModal] = useState(true);
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
 
     useEffect(() => {
-        // Check local storage for modal preference and timestamp
-        const modalPreference = localStorage.getItem("modalTimestamp");
-        if (modalPreference) {
-            const lastClosedTime = new Date(modalPreference);
-            const currentTime = new Date();
-            const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
-            
-            if (currentTime - lastClosedTime < oneDayInMilliseconds) {
-                // Less than one day has passed, do not show modal
-                setShowModal(false);
-            } else {
-                // More than one day has passed, show modal
-                onOpen();
-            }
-        } else {
-            // No preference found, show modal
-            onOpen();
-        }
-        
         getAllPins();
     }, []);
 
@@ -56,16 +35,6 @@ export default function Home() {
         }
     }
 
-    const handleDoNotShowAgain = () => {
-        // Set current time in local storage
-        localStorage.setItem("modalTimestamp", new Date().toISOString());
-    };
-
-    const handleok = () => {
-        // Set current time in local storage
-        setShowModal(false);
-        onOpenChange(false);
-    };
 
     return (
         <>
@@ -76,31 +45,6 @@ export default function Home() {
                 />
             </div>
 
-            {showModal && (
-                <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-                    <ModalContent>
-                        {(onClose) => (
-                            <>
-                                <ModalHeader className="flex flex-col gap-1">ประกาศ!!</ModalHeader>
-                                <ModalBody>
-                                    <p>
-                                        ให้ไปที่ Edit Profile สำหรับทุกคนที่ยังไม่ได้เพิ่ม ข้อมูลรหัสนักศึกษา และ Section ก่อนทำการสร้างโพสต์
-                                    </p>
-                                </ModalBody>
-                                <ModalFooter className="flex flex-col items-stretch gap-2">
-                                    <div className="flex items-center justify-between">
-                                        <Checkbox onValueChange={handleDoNotShowAgain}>ไม่แสดงหน้านี้อีก</Checkbox>
-                                        <Button color="primary" onPress={handleok} aria-label="ok button"
-                                        >
-                                            OK
-                                        </Button>
-                                    </div>
-                                </ModalFooter>
-                            </>
-                        )}
-                    </ModalContent>
-                </Modal>
-            )}
         </>
     );
 }
